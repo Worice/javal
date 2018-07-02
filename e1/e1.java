57113d9e63891b1c4d973c2c1a88bb78ebd355a9
Worice <maurizio.murino@hotmail.com>
2018-07-02 08:30:31 +0000

    Exercises up to 14, chapter 1

---
+++ e1/e1.java
@@ -0,0 +1,46 @@
+// Esercizi di programmazione java capitolo 1
+
+import javax.swing.JOptionPane;
+
+public class e1{
+	public static void main(String [] args){
+/*1*/		System.out.println("Guten morgen!");
+/*2*/		System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
+/*3*/		System.out.println(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10);
+/*4*/		System.out.print("Primo anno: 1000 + 5% ");	
+		System.out.println(1000 * 1.05);
+/*4*/		System.out.print("Secondo anno: ans + 5% ");	
+		System.out.println((1000 * 1.05) * 1.05);
+/*4*/		System.out.print("Terzo anno: ans + 5% ");	
+		System.out.println(((1000 * 1.05) * 1.05) * 1.05);
+/*5*/		System.out.println("+--------+");
+		System.out.println("|Maurizio|");
+		System.out.println("+--------+");
+/*6*/		System.out.println("*   *    *    *   * ***  *");
+		System.out.println("** **   * *   *   * *  * *");
+		System.out.println("* * *  *****  *   * ***  *");
+		System.out.println("*   * *     * ***** *  * *");
+/*7*/		System.out.println("__ ._ ..._ ._. ..");
+/*8*/		System.out.println(":)");
+/*9*/		System.out.println("|||||||||||");
+		System.out.println("|*+++++++*|");
+		System.out.println("|**+++++**|");
+		System.out.println("|***+++***|");
+		System.out.println("|****+****|");
+		System.out.println("|||||||||||");
+/*10*/		System.out.println(" - ");
+		System.out.println("- -");
+		System.out.println("| |");
+/*11*/		System.out.println("(:= ;hello young coder!");
+/*12*/		System.out.print("Paolo ");
+		System.out.print("Paola ");
+		System.out.println("Paulu");
+/*13*/		System.out.println("M'illumino d'immenso");
+/*14*/		System.out.println("*****=================");
+		System.out.println("*****=================");
+		System.out.println("*****=================");
+		System.out.println("======================");
+		System.out.println("======================");
+		System.out.println("======================");
+	}
+}	
