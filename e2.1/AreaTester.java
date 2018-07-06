b213fa183ac8172684d6edb4a4f6b203a67195dc
Worice <maurizio.murino@hotmail.com>
2018-07-06 13:41:57 +0000

    Exercise e2.1

---
+++ e2.1/AreaTester.java
@@ -0,0 +1,8 @@
+import java.awt.Rectangle;
+
+public class AreaTester{
+	public static void main(String[] args){
+		Rectangle box = new Rectangle(20, 20, 50, 50);
+		System.out.println(box.getWidth() * box.getHeight());
+	}
+}
